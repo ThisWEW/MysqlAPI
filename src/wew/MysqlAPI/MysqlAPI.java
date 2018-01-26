1b2eff6df54d08fb1c2e3d4163dc19e506f70a61
Eas <1281243566@qq.com>
2018-01-26 14:26:12 +0800

    开发 v1.6
    新增 onUpdate()方法 (有使用注释)，现在可以更方便的更新数据了

--- src/wew/MysqlAPI/MysqlAPI.java
+++ src/wew/MysqlAPI/MysqlAPI.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -138,4 +139,32 @@ public class MysqlAPI {
 			}
 			return builder.toString();
 		}
+		
+		/**
+		 * key = 主键列名
+		 * keydata = 主键的名称
+		 * value = 需要修改的列名
+		 * valuedata = 修改的值
+		 * @param table
+		 * @param key
+		 * @param keydata
+		 * @param value
+		 * @param valuedata
+		 */
+		public void onUpdate(String table, String key, String keydata, String value, Object valuedata) {
+			Statement stmt = null;
+			try {
+				stmt = connect.createStatement();
+				ResultSet res = stmt.executeQuery("select 1 from " + table + " where " + key + "='" + keydata + "'");
+				if(!res.next()) {
+					stmt.executeUpdate("insert into " + table + " values('" + keydata + "','" + valuedata + "')");
+				} else {
+					res = stmt.executeQuery("select " + value + " from " + table + " where " + key + "='" + keydata + "'");
+					stmt.executeUpdate("update " + table + " set " + value + "='" + valuedata + "' where " + key + "='" + keydata + "'");
+				}
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+	    }
 }
